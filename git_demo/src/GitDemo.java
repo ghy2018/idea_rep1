13a5091458064474ac9f3bbe2ddc707c0cebc49d
ghy <guohuanyang@itcast.cn>
2019-05-13 12:58:18 +0800

    初次使用idea提交代码到Git

---
+++ git_demo/src/GitDemo.java
@@ -0,0 +1,12 @@
+/**
+ * @Company www.itheima.com
+ * @Author itheima
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
